c1cf6ef17b1b4d7edee5b40fadfeaef900af0ba7
Dinara <aduvantt@gmail.com>
2024-12-10 21:45:22 +0500

    Update InputSumCheck.java

--- InputSumCheck.java
+++ InputSumCheck.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Lesson2 {
+public class InputSumCheck{
     public static void main(String[] args) {
         //5
         boolean checkResult = inputSumCheck();
