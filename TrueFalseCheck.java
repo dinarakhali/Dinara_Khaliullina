9bb38572fe8f6ed35cb078669e6b4381caacf673
Dinara <aduvantt@gmail.com>
2024-12-10 21:47:20 +0500

    Update TrueFalseCheck.java

--- TrueFalseCheck.java
+++ TrueFalseCheck.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Lesson2 {
+public class TrueFalseCheck{
     public static void main(String[] args) {
         //7
         boolean result = trueFalseCheck(10);
