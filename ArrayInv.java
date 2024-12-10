e9748b63ca1b41d6812950173de71eed959286b0
Dinara <aduvantt@gmail.com>
2024-12-10 21:50:24 +0500

    Update ArrayInv.java

--- ArrayInv.java
+++ ArrayInv.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Lesson2 {
+public class ArrayInv{
     public static void main(String[] args) {
         //10
         arrayInv();
