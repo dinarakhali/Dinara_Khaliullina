41dae441ddc605d9b722c0f2dea7d82e67c54fce
Dinara <aduvantt@gmail.com>
2024-12-10 21:55:22 +0500

    Update AllMethodsInOne.java

--- AllMethodsInOne.java
+++ AllMethodsInOne.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Lesson2 {
+public class AllMethodsInOne{
     public static void main(String[] args) {
         //1
         printThreeWords();
