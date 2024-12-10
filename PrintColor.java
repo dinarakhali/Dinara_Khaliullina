1a2fa568a351ceba78ad15a1b7e2deaafc37f10b
Dinara <aduvantt@gmail.com>
2024-12-10 21:43:53 +0500

    Update PrintColor.java

--- PrintColor.java
+++ PrintColor.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Lesson2 {
+public class PrintColor{
     public static void main(String[] args) {
         //3
         printColor();
