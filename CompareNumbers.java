408283257b6de9b2166932ab13b64888c7e46dda
Dinara <aduvantt@gmail.com>
2024-12-10 21:44:40 +0500

    Update CompareNumbers.java

--- CompareNumbers.java
+++ CompareNumbers.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Lesson2 {
+public class CompareNumbers{
     public static void main(String[] args) {
         //4
         compareNumbers();
