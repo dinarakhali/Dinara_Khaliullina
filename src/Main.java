898ecbf699d39d64bff2feea9c498bec287fe621
Dinara Khali <aduvantt@gmail.com>
2025-01-16 17:24:31 +0500

    Initial commit for Lesson_12

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,49 @@
 public class Main {
     public static void main(String[] args) {
-        String[][] array = {{"1", "10", "Елка", ":)"}, {"Снег", "20", "3", "2"}, {"2", "2", "2", "2"}, {"1", "1", "1", "1"}};
-        String[][] wrongSizeArray = {{"1", "10", "3"}, {"20", "3", "2"}};
+        String[][] array = {
+                {"1", "10", "Елка", ":)"},
+                {"Снег", "20", "3", "2"},
+                {"2", "2", "2", "2"},
+                {"1", "1", "1", "1"}};
+        String[][] wrongSizeArray = {
+                {"1", "10", "3"},
+                {"20", "3", "2"}};
+        String[][] rightArray = {
+                {"2", "5", "3", "1"},
+                {"2", "5", "3", "1"},
+                {"2", "5", "3", "1"},
+                {"2", "5", "3", "1"}
+        };
 
         try {
             int summ = transformArray(array);
             System.out.println("Сумма элементов массива = " + summ + "\n");
         } catch (MyArraySizeException e) {
-            System.err.println("Ошибка: " + e.getMessage());
+            System.err.println(e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.err.println(e.getMessage());
         }
 
         try {
             int summ = transformArray(wrongSizeArray);
             System.out.println("Сумма элементов массива = " + summ + "\n");
         } catch (MyArraySizeException e) {
-            System.err.println("Ошибка: " + e.getMessage());
+            System.err.println(e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.err.println(e.getMessage());
+        }
+
+        try {
+            int summ = transformArray(rightArray);
+            System.out.println("Сумма элементов массива = " + summ + "\n");
+        } catch (MyArraySizeException e) {
+            System.err.println(e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.err.println(e.getMessage());
         }
     }
 
-    public static int transformArray(String[][] array) throws MyArraySizeException {
+    public static int transformArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
         if (array.length != 4) {
             throw new MyArraySizeException("Ошибка: Массив должен быть размером 4x4.");
         }
@@ -34,7 +59,7 @@ public class Main {
                 try {
                     summ += Integer.parseInt(array[i][j]);
                 } catch (NumberFormatException e) {
-                    System.err.println("Ошибка: Некорректные данные в ячейке [" + i + "][" + j + "]");
+                    throw new MyArrayDataException("Ошибка: Некорректные данные в ячейке [" + i + "][" + j + "]");
                 }
             }
         }
