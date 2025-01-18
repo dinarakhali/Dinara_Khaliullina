6aa18b951445e123044f60578d1b73472ba77f87
Dinara Khali <aduvantt@gmail.com>
2025-01-18 11:58:58 +0500

    Initial commit for hw

--- src/main/java/Factorial.java
+++ src/main/java/Factorial.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
-
 public class Factorial {
+    public static int getFactorial(int f) throws NegativeNumberException, BigNumberException {
+        if (f < 0) {
+            throw new NegativeNumberException("Число не может быть отрицательным.");
+        } else if (f <= 1) {
+            return 1;
+        } else if (f > 16) {
+            throw new BigNumberException("Для чисел больше 16 напишите метод с типом BigInteger.");
+        } else {
+            return f * getFactorial(f - 1);
+        }
+    }
+
+    /* public static void main(String[] args) {
+        try {
+            int result = getFactorial(-5);
+            System.out.println("Факториал = " + result);
+        } catch (NegativeNumberException | BigNumberException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            int result = getFactorial(17);
+            System.out.println("Факториал = " + result);
+        } catch (NegativeNumberException |
+                 BigNumberException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            int result = getFactorial(8);
+            System.out.println("Факториал = " + result);
+        } catch (NegativeNumberException |
+                 BigNumberException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    */
 }
--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FactorialTest {
+
+    @Test
+    @DisplayName("Тестирование факториала - эквивалентное разбиение и границы для int.")
+
+    public void factorialTest() throws NegativeNumberException, BigNumberException {
+        assertEquals(40320, Factorial.getFactorial(8));
+        assertEquals(1, Factorial.getFactorial(0));
+        assertEquals(1, Factorial.getFactorial(1));
+        assertEquals(2004189184, Factorial.getFactorial(16));
+    }
+
+    @Test
+    @DisplayName("Тестирование факториала - исключения.")
+
+    public void factorialExcTest() {
+        assertThrows(NegativeNumberException.class, () -> {
+            Factorial.getFactorial(-4);
+        });
+
+        try {
+            Factorial.getFactorial(21);
+            fail("Исключение не брошено");
+        } catch (NegativeNumberException | BigNumberException e) {
+        }
+    }
 }
